630d5c186b2d2560ea43fba5a44992041293eefc
wenzubot <emailkeamanann@gmail.com>
2025-10-04 11:07:16 +0700

    Add settings.js

---
+++ settings.js
@@ -0,0 +1,76 @@
+// Script By RafatharCode 
+// Base By RafatharCode 
+// Buy Script > Tele: t.me/RafatharCode
+/* # Penting
+   Jangan hapus credits atau nama developer
+   hargai pembuat script ini!*/
+
+const settings = {
+  version: 'auto order v4',
+  githubtoken: '-',
+  repoowner: '-',
+  reponame: '-',
+  filepath: '-',
+  owner: '', // ganti id telegram lu
+  token: '', // Token Bot Lu
+  // Token & Bash
+tokeninstall: 'alfibandung',
+bash: 'bash <(curl https://raw.githubusercontent.com/RafatharUserbotV4/Rafaxamalia/main/install.sh)',
+ // Vps Digitalocean 
+ apiDO: '-',
+ // Panel 
+ loc: '1',
+ eggs: '15',
+ nestid: '5',
+ domain: '',
+ apikey: '',
+ capikey: '',
+ domainv2: 'https://',
+ apikeyv2: '-',
+ capikeyv2: '-',
+ pepe: 'https://files.catbox.moe/k2fxyq.jpeg',
+ // Atlantik 
+ApikeyAtlantic: "", // Apikey Atlantik Lu Isi
+FeeTransaksi: 100, // Fee Isi Sesuai Aja
+
+nomor_pencairan: "", // Nomor Pencairan Mu
+type_ewallet: "", // dana/ovo/gopay
+atas_nama_ewallet: "", // atas nama ewallet
+  //~~~~~~~~~ Settings Orderkuota ~~~~~~~~//
+merchantidorderkuota:'-',
+apiorderkuota: '-',
+qrisorderkuota: '-',
+pinorkut: '-',
+pworkut: '-',
+pinh2h: '-',
+passwordh2h: '-',
+apisimplebot: 'alfibandung',
+restapi: '-',
+dana: '-',
+ovo: '-',
+gopay: '-',
+bni: '-',
+// Google Ai
+apikeygoogle: 'AIzaSyATBCMkHF-j_I6TzTDdpDNhQMx_34euCzA',
+//Subdomain 
+zonetld1: 'ea719beeec3cfe39b58f0195f848498f',
+apitokentld1: 'Gb8j0xFasrWB1k80b4BFrIL_f2IgAQ5n66CamFbP',
+domaintld1: 'panelku-ptero.my.id',
+zonetld2: '8211830a7911e9028f38018243ea360d',
+apitokentld2: 'XLllrPcIOIC-S4yQP2iaJ9GELQVrL5agcAtEyGXN',
+domaintld2:'sainsproject.biz.id',
+zonetld3: '05478e906fa1556f81ae0eaf86816060',
+apitokentld3: 'nkIplLsfGW-FSUYEpbSt3_I2-a9JIWZIvGO5W6xN',
+domaintld3: 'barmodsdomain.my.id',
+zonetld4: 'b1b16801d28009e899a843b0c8faee34',
+apitokentld4: 'y_0WKCNCnOgx0sgbcQr-puVTXyTQPN9KErR9vlzN',
+domaintld4: 'publicserver.my.id',
+zonetld5: '082ec80d7367d6d4f7c52600034ac635',
+apitokentld5: 'r3XUyNYtxNQYwZtGUIAChRqe0uTzwV4eVO7JpJ_l',
+domaintld5: 'rikionline.shop',
+zonetld6: 'c651c828a01962eb3c530513c7ad7dcf',
+apitokentld6: 'N-D6fN6la7jY0AnvbWn9FcU6ZHuDitmFXd-JF04g',
+domaintld6: 'storeid.my.id',
+};
+
+module.exports = settings;
