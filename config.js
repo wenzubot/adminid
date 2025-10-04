de051c812d008ac7e2413e19b14be03941885dde
wenzubot <emailkeamanann@gmail.com>
2025-10-04 09:24:20 +0700

    Add config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    tokens: "8464758855:AAG-KIiEOjRAIK9oKta6BX7GT4GFKjAO7qA", // ubah jadi token bot mu
+    owner: "8295141776", // ubah jadi id tele mu
+    port: "2097", // ubah jadi port panel mu
+    ipvps: "152.42.212.66" // ubah jadi ipvps mu
+};
